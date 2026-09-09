755a6def39c267f1d28c7a1a85d711c02776de7c
dimamil <dmil.dev@icloud.com>
2026-09-08 21:38:51 -0400

    Allowed Hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["186.246.49.80"]
 
 
 # Application definition
